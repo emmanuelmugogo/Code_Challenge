102b6eacd0ec774ac6dc3f4a9071313e5db4749c
Emmanuel <Emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-31 09:27:59 -0400

    Added a logic to remove a last period on each word

--- src/main/java/WordsCounter.java
+++ src/main/java/WordsCounter.java
@@ -12,6 +12,42 @@ import java.util.Map;
 
 public class WordsCounter {
 	
+	
+	// Main method
+	public static void main (String [] args ) {
+		
+		//File path as it is stored to my computer
+		String filePath = "/Users/Emmanuel/Downloads/Interview/Paragraph.txt";  
+		 
+        System.out.println( "**********************************\nParagraph as it Reads before sorting into words counter \n\n\n" +
+        		readFileFromPath( filePath ) + "\n\n\nWords Counter starts here\n**********************************\n\n" );
+		
+	
+		String sentence = readFileFromPath( filePath );	
+		sentence = sentence.toUpperCase().replaceAll("[-\"|\"$:?,\t\n\r()\\s+]"," ");
+		//replacing the last period on words.
+		
+		String[] sentenceArray = sentence.split(" ");
+		Map<String, Integer> myMap = new HashMap<String, Integer>();
+		
+		// putting counters into the Map values and use words as keys.
+		for (int i = 0; i < sentenceArray.length; i++) {
+			myMap.put(sentenceArray[i], returnCounter(sentence, sentenceArray[i]));
+		}
+		
+		// Sorting a map and put to mySortedMap
+		Map<String, Integer> mySorterMap = sortByValue(myMap); 
+		  
+        // Printing a Sorted map. 
+        for (Map.Entry<String, Integer> en : mySorterMap.entrySet()) { 
+        	if(en.getKey().length() >= 1) {    // checking the length to remove the white spaces printing.
+        		System.out.println(en.getKey() + "\t-\t " + en.getValue());
+        	}
+             
+        } 
+			 
+	}
+	
 	//Read file content into String
     static String readFileFromPath(String filePath)
     {
@@ -62,40 +98,17 @@ public class WordsCounter {
             temp.put(aa.getKey(), aa.getValue()); 
         } 
         return temp; 
-    } 
+    }
     
-	// Main method
-	public static void main (String [] args ) {
-		
-		//File path to my local computer
-		String filePath = "/Users/Emmanuel/Downloads/Interview/Paragraph.txt";  
-		 
-        System.out.println( "**********************************\nParagraph as it Reads before sorting into words counter \n\n\n" +
-        		readFileFromPath( filePath ) + "\n\n\nWords Counter starts here\n**********************************\n\n" );
-		
-	
-		String sentence = readFileFromPath( filePath );	
-		sentence = sentence.toUpperCase().replaceAll("[-\"|\"$:?,\t\n\r()\\s+]"," ");
-		String[] sentenceArray = sentence.split(" ");
-		Map<String, Integer> myMap = new HashMap<String, Integer>();
-		
-		// putting counters into the Map values and use words as keys.
-		for (int i = 0; i < sentenceArray.length; i++) {
-			myMap.put(sentenceArray[i], returnCounter(sentence, sentenceArray[i]));
-		}
-		
-		// Sorting a map and put to mySortedMap
-		Map<String, Integer> mySorterMap = sortByValue(myMap); 
-		  
-        // Printing a Sorted map. 
-        for (Map.Entry<String, Integer> en : mySorterMap.entrySet()) { 
-        	if(en.getKey().length() >= 1) {    // checking the length to remove the white spaces printing.
-        		System.out.println(en.getKey() + "\t-\t " + en.getValue());
-        	}
-             
-        } 
-			
-		 
-	}
+    // Logic to replace last period (.) on a word
+    static String replaceLastChar(String stringWord, String toReplace, String replacement) {
+    	
+    	int ind = stringWord.lastIndexOf(toReplace);
+    	if( ind>=0 ) {
+    		stringWord = new StringBuilder(stringWord).replace(ind, ind+1,replacement).toString();
+    	}
+    	
+    	return stringWord;
+    }
 
 }
